a5276324d7f2232dbf392abcafbb2d93944f7c14
Adebayo Sotannde <me@adebayosotannde.com>
2025-12-11 07:11:34 -0600

    Added the Subcription View controler and View

---
+++ SkyLink/SubscriptionManager.swift
@@ -0,0 +1,24 @@
+//
+//  SubscriptionManager.swift
+//  SkyLink
+//
+//  Created by Adebayo Sotannde on 10/22/25.
+//
+
+import Foundation
+import FirebaseAuth
+import FirebaseFirestore
+
+final class SubscriptionManager
+{
+    static let shared = SubscriptionManager()
+    private init() {}
+
+   
+    func isSubcribed() -> Bool
+    {
+        return false
+    }
+
+   
+}
---
+++ SkyLink/ViewController/SubscriptionPlanView.swift
@@ -0,0 +1,8 @@
+//
+//  SubscriptionPlanView.swift
+//  SkyLink
+//
+//  Created by Adebayo Sotannde on 12/11/25.
+//
+
+import Foundation
